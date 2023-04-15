db953906e87073fa804455fa63166117e8bd6b08
Richard Basson <dvtrb@utilita.co.uk>
2023-04-15 18:38:57 +0200

    Added gallary

---
+++ MJGalary.Web/MessageResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MJGalary.Web.Client
+{
+    public class MessageResponse
+    {
+        public string Id { get; set; }
+        public string ImageId { get; set; }
+        public string Prompt { get; set; }
+        public string Url { get; set; }
+        public string Category { get; set; }
+        public DateTimeOffset TimeStamp { get; set; }
+    }
+}
